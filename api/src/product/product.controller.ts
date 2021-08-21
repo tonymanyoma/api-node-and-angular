246bcfaa41a50a9c2cfa05296020198e3deda802
Antony <tonymanyoma5@gmail.com>
2021-08-20 20:41:40 -0500

    add files

---
+++ api/src/product/product.controller.ts
@@ -0,0 +1,54 @@
+import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
+import { get } from 'http';
+import { CreateProductDTO } from './dto/product.dto'
+
+import { ProductService } from './product.service'
+@Controller('product')
+export class ProductController {
+
+    constructor(private productService: ProductService) {}
+
+    @Post('/create')
+    async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+        const product = await this.productService.createProduct(createProductDTO);
+       return res.status(HttpStatus.OK).json({
+            message: 'Producto creado con éxito',
+            product: product
+        });
+    }
+
+    @Get('/')
+    async getProducts(@Res() res) {
+       const products = await this.productService.getProducts();
+       return res.status(HttpStatus.OK).json({
+           products: products
+       })
+    }
+
+    @Get('/:productID')
+    async getProduct(@Res() res, @Param('productID') productID) {
+       const product = await this.productService.getProduct(productID);
+       if (!product) throw new NotFoundException('El producto no existe');
+       return res.status(HttpStatus.OK).json(product)
+    }
+
+    @Delete('/delete')
+    async deleteProduct(@Res() res, @Query('productID') productID) {
+       const product = await this.productService.deleteProduct(productID);
+       if (!product) throw new NotFoundException('El producto no existe');
+       return res.status(HttpStatus.OK).json({
+           message: 'Producto eliminado con éxito',
+           product: product
+       })
+    }
+
+    @Put('/update')
+    async updateProduct(@Res() res,@Body() createProductDTO: CreateProductDTO, @Query('productID') productID) {
+       const updatedProduct = await this.productService.updateProduct(productID, createProductDTO);
+       if (!updatedProduct) throw new NotFoundException('El producto no existe');
+       return res.status(HttpStatus.OK).json({
+           message: 'Producto actualizado con éxito',
+           updatedProduct: updatedProduct
+       })
+    }
+}
---
+++ front/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddProductComponent,
+    ProductDetailsComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
