296db405089db4da971c2ad4fef216e29d4d7682
Antony <tonymanyoma5@gmail.com>
2021-08-21 14:54:29 -0500

    conexion a mongodb atlas

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule} from '@nestjs/mongoose'
 import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [ProductModule, GameModule, HttpModule ,MongooseModule.forRoot('mongodb://localhost/products', {
+  imports: [ProductModule, GameModule, HttpModule ,MongooseModule.forRoot('mongodb+srv://antony:hbVr0cC86ZdhW5Gh@cluster0.jcvo5.mongodb.net/products?retryWrites=true&w=majority&ssl=true', {
     useNewUrlParser: true
   })],
   controllers: [AppController],
